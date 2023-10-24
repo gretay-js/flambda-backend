3de4efbe23c3c8d3423e8e2937579422cc299033
Greta Yorsh <gyorsh@janestreet.com>
2023-10-24 15:42:51 +0100

    Add a test

--- tests/backend/checkmach/t.ml
+++ tests/backend/checkmach/t.ml
@@ -261,3 +261,26 @@ end
 
 let[@zero_alloc assume never_returns_normally strict] test60 x = (x,x)
 let[@zero_alloc assume strict never_returns_normally] test61 x = (x,x)
+
+let[@inline always] test42 x = (x,x)
+
+let[@zero_alloc] test43 x =
+  (test42[@zero_alloc assume]) x
+
+
+let[@inline never] test44 x = (x,x)
+
+let[@zero_alloc] test45 x =
+  (test44[@zero_alloc assume]) x
+
+let[@inline always] test46 x = if x > 0 then failwith (Printf.sprintf "%d" x) else (x,x)
+
+let[@zero_alloc strict] test47 x =
+  (test46[@zero_alloc assume strict]) x
+
+let[@zero_alloc strict] test50 x =
+  (test46[@zero_alloc assume never_returns_normally strict]) x
+
+let[@zero_alloc] test51 x =
+  try (test46[@zero_alloc assume never_returns_normally]) x
+  with _ -> failwith (Printf.sprintf "%d" x)
