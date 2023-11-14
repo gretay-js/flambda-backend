a0acf9bd59f50d5a6d6fe88740c57ad9c164b607
Greta Yorsh <gyorsh@janestreet.com>
2023-11-14 13:41:34 +0000

    Remove comment that is no longer relevant

--- backend/cfg/cfg_simplify.ml
+++ backend/cfg/cfg_simplify.ml
@@ -276,9 +276,7 @@ end = struct
 end
 
 let run cfg_with_layout =
- (* note: the simplification of terminators is necessary for the equality. The
-     other code path simplifies e.g. a switch with three branches into an
-     integer test. This simplification should happen *after* the one about
+ (* note: This simplification should happen *after* the one about
      straightline blocks because merging blocks creates more opportunities for
      terminator simplification. *)
   Eliminate_fallthrough_blocks.run cfg_with_layout;
