7936aed1abfc75629dfcd9e580cd6b28194e1900
Greta Yorsh <gyorsh@janestreet.com>
2023-12-27 14:45:59 +0000

    Add a test for never_returns_normally on Alloc

--- tests/backend/checkmach/test_assume_fail.ml
+++ tests/backend/checkmach/test_assume_fail.ml
@@ -18,8 +18,34 @@ let[@zero_alloc strict] test48 x =
 
 (* Perhaps confusingly, never_returns_normally works on allocations not only
    on calls. This is needed for analysis to give the same results regardess
-   of inlining of expressions annotated with "assume".  *)
+   of inlining of expressions annotated with "assume". See test50-53 below.  *)
 let[@zero_alloc] test49 x =
   try let y = (test46[@zero_alloc assume never_returns_normally]) x in [y;(x,x+1)]
   with _ -> failwith (Printf.sprintf "%d" x)
 
+(* strict check should fail *)
+let[@inline always][@zero_alloc assume never_returns_normally] test50 x = (x, x)
+
+let[@zero_alloc strict] test51 x = test50 x
+
+let[@inline never][@zero_alloc assume never_returns_normally] test52 x = (x, x)
+
+let[@zero_alloc strict] test53 x = test52 x
+
+(* relaxed check should pass *)
+let[@inline always][@zero_alloc assume never_returns_normally] test54 x = (x, x)
+
+let[@zero_alloc] test55 x = test54 x
+
+let[@inline never][@zero_alloc assume never_returns_normally] test56 x = (x, x)
+
+let[@zero_alloc] test57 x = test56 x
+
+(* strict assume should pass *)
+let[@inline always][@zero_alloc assume strict never_returns_normally] test58 x = (x, x)
+
+let[@zero_alloc strict] test59 x = test58 x
+
+let[@inline never][@zero_alloc assume never_returns_normally strict] test60 x = (x, x)
+
+let[@zero_alloc strict] test61 x = test60 x
