c061effbb8337279024bc978676b61281a867ad8
Greta Yorsh <gyorsh@janestreet.com>
2024-01-18 13:48:41 +0000

    Actually eliminate dead handlers

--- backend/cfg/cfg_dataflow.ml
+++ backend/cfg/cfg_dataflow.ml
@@ -360,7 +360,7 @@ module Forward (D : Domain_S) (T : Forward_transfer with type domain = D.t) :
     let work_state =
       Dataflow_impl.create cfg
         ~init:(fun block ->
-          if Label.equal block.start cfg.entry_label || block.is_trap_handler
+          if Label.equal block.start cfg.entry_label
           then Some init
           else None)
         ~store_instr:false
