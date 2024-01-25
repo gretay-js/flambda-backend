005b8420c52dcbebd3fb75a053ba7051a916f0ab
Greta Yorsh <gyorsh@janestreet.com>
2024-01-25 19:30:36 +0000

    Comment about the meaning of zero_alloc assume payload

--- ocaml/lambda/assume_info.ml
+++ ocaml/lambda/assume_info.ml
@@ -1,6 +1,37 @@
 type t =
   | No_assume
   | Assume of { strict : bool; never_returns_normally : bool }
+(*
+
+   The meaning of keywords [strict] and [never_returns_normally]
+   is defined in terms of abstract values as follows:
+
+   relaxed (default):         nor = Safe and exn = Top  and div = Top
+   strict:                    nor = Safe and exn = Safe and div = Safe
+   never_returns_normally:    nor = Bot  and exn = Top  and div = Top
+
+   where [nor] means normal return of the call, [exn] means return via an exception,
+   [div] means diverging (non-terminating) executions,
+   and the meaning and order of elements is:
+
+   Top    may allocate
+   Safe   does not allocate on any execution paths
+   Bot    unreachable
+
+   Using more than one keyword means intersection (i.e., meet of the  elements,
+   pointwise lifted to tuples), so we get the following:
+
+   [@zero_alloc assume]                               nor = Safe and exn = Top  and div = Top
+   [@zero_alloc assume strict]                        nor = Safe and exn = Safe and div = Safe
+   [@zero_alloc assume strict never_returns_normally] nor = Bot  and exn = Safe and div = Safe
+   [@zero_alloc assume never_returns_normally]        nor = Bot  and exn = Top  and div = Top
+
+   See [Value] and [Annotation] in [backend/checkmach.ml].
+*)
+(* CR gyorsh: should we move [Value] and [Annotation] here or maybe "utils" and use them
+   directly, instead of the weird compare function that abstracts them? Perhaps we
+   should translate "strict" and "never_returns_normally" directly into (nor,exn,div)
+*)
 
 let[@inline always] rank = function
   | No_assume -> 0
