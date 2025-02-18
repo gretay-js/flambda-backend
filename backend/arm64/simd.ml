642e1b2421740de22bee0e68428453983295715f
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-02-18 16:02:24 +0000

    arm64 intrinsics for float32

--- backend/arm64/simd.ml
+++ backend/arm64/simd.ml
@@ -20,40 +20,69 @@ open Format
 
 type operation_class = Pure
 
-type rounding_mode =
-  | Current
-  | Neg_inf
-  | Pos_inf
-  | Zero
+module Rounding_mode = struct
+  type t =
+    | Current
+    | Neg_inf
+    | Pos_inf
+    | Zero
+
+  let instruction_suffix = function
+    | Neg_inf -> "m"
+    | Pos_inf -> "p"
+    | Zero -> "z"
+    | Current -> "x"
+
+  let print_rounding_mode ppf mode =
+    let suffix = instruction_suffix mode in
+    Format.pp_print_string ppf suffix
+
+  let equal t1 t2 =
+    match t1, t2 with
+    | Current, Current | Neg_inf, Neg_inf | Pos_inf, Pos_inf | Zero, Zero ->
+      true
+    | (Current | Neg_inf | Pos_inf | Zero), _ -> false
+end
 
 type operation =
-  | Round_f32 of rounding_mode
+  | Round_f32 of Rounding_mode.t
   | Round_f32_i64
+  | Min_scalar_f32
+  | Max_scalar_f32
 
-let rounding_mode_instruction_suffix = function
-  | Neg_inf -> "m"
-  | Pos_inf -> "p"
-  | Zero -> "z"
-  | Current -> "x"
+let instr_size op =
+  match op with
+  | Round_f32 _ | Round_f32_i64 | Min_scalar_f32 | Max_scalar_f32 -> 1
 
-let print_rounding_mode ppf mode =
-  let suffix = rounding_mode_instruction_suffix mode in
-  Format.pp_print_string ppf suffix
+let emit_opcode op =
+  match op with
+  | Round_f32 rounding_mode ->
+    "frint" ^ Rounding_mode.instruction_suffix rounding_mode
+  | Round_f32_i64 -> "fcvtns"
+  | Min_scalar_f32 -> "fmin"
+  | Max_scalar_f32 -> "fmax"
 
 let print_operation printreg op ppf arg =
   match op with
   | Round_f32 mode ->
-    fprintf ppf "frint%a %a %a" print_rounding_mode mode printreg arg.(0)
-      printreg arg.(1)
+    fprintf ppf "frint%a %a %a" Rounding_mode.print_rounding_mode mode printreg
+      arg.(0) printreg arg.(1)
   | Round_f32_i64 ->
     fprintf ppf "fcvtzs %a %a" printreg arg.(0) printreg arg.(1)
+  | Min_scalar_f32 ->
+    fprintf ppf "min_scalar_f32 %a %a" printreg arg.(0) printreg arg.(1)
+  | Max_scalar_f32 ->
+    fprintf ppf "max_scalar_f32 %a %a" printreg arg.(0) printreg arg.(1)
 
 let equal_operation op1 op2 =
   match op1, op2 with
-  | Round_f32 mode, Round_f32 mode' -> mode = mode'
+  | Round_f32 mode, Round_f32 mode' -> Rounding_mode.equal mode mode'
   | Round_f32_i64, Round_f32_i64 -> true
-  | Round_f32 _, _ | Round_f32_i64, _ -> false
+  | Min_scalar_f32, Min_scalar_f32 | Max_scalar_f32, Max_scalar_f32 -> true
+  | (Round_f32 _ | Round_f32_i64 | Min_scalar_f32 | Max_scalar_f32), _ -> false
 
-let class_of_operation op = match op with Round_f32 _ | Round_f32_i64 -> Pure
+let class_of_operation op =
+  match op with
+  | Round_f32 _ | Round_f32_i64 | Min_scalar_f32 | Max_scalar_f32 -> Pure
 
 let operation_is_pure op = match class_of_operation op with Pure -> true
--- backend/arm64/simd_selection.ml
+++ backend/arm64/simd_selection.ml
@@ -19,11 +19,13 @@ open Simd
 
 let select_simd_instr op args =
   match op with
-  | "caml_neon_float32_round_neg_inf" -> Some (Round_f32 Neg_inf, args)
-  | "caml_neon_float32_round_pos_inf" -> Some (Round_f32 Pos_inf, args)
-  | "caml_neon_float32_round_towards_zero" -> Some (Round_f32 Zero, args)
-  | "caml_neon_float32_round_current" -> Some (Round_f32 Current, args)
-  | "caml_neon_cast_float32_int64" -> Some (Round_f32_i64, args)
+  | "caml_simd_float32_round_neg_inf" -> Some (Round_f32 Neg_inf, args)
+  | "caml_simd_float32_round_pos_inf" -> Some (Round_f32 Pos_inf, args)
+  | "caml_simd_float32_round_towards_zero" -> Some (Round_f32 Zero, args)
+  | "caml_simd_float32_round_current" -> Some (Round_f32 Current, args)
+  | "caml_simd_cast_float32_int64" -> Some (Round_f32_i64, args)
+  | "caml_simd_float32_min" -> Some (Min_scalar_f32, args)
+  | "caml_simd_float32_max" -> Some (Max_scalar_f32, args)
   | _ -> None
 
 let select_operation op args =
