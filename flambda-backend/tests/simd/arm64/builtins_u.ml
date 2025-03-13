052f9a21f4390e09cee3fe2a2b95f1155af29f28
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-03-13 16:46:24 +0000

    fix tests

--- flambda-backend/tests/simd/arm64/builtins_u.ml
+++ flambda-backend/tests/simd/arm64/builtins_u.ml
@@ -865,3 +865,18 @@ module SSE42_String = struct
     (int[@untagged]) = "caml_vec128_unreachable" "caml_sse42_vec128_cmpistrz"
     [@@noalloc] [@@builtin]
 end
+
+module Float_interleave = struct
+  (* Re-exposes SSE and SSE2 intrinisics with a different type. *)
+  external interleave_low_32 : float32x4 -> float32x4 -> float32x4
+    = "caml_vec128_unreachable" "caml_sse_vec128_interleave_low_32"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external interleave_low_64s : float32x4 -> float32x4 -> float32x4
+    = "caml_vec128_unreachable" "caml_sse2_vec128_interleave_low_64"
+    [@@noalloc] [@@unboxed] [@@builtin]
+
+  external interleave_low_64 : float64x2 -> float64x2 -> float64x2
+    = "caml_vec128_unreachable" "caml_sse2_vec128_interleave_low_64"
+    [@@noalloc] [@@unboxed] [@@builtin]
+end
