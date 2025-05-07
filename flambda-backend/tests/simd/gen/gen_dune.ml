51bb0f424127a0c1b31d9679ad8d0c1b61a13e73
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-05-07 17:50:19 +0100

    split float32x4 out of ops.ml

--- flambda-backend/tests/simd/gen/gen_dune.ml
+++ flambda-backend/tests/simd/gen/gen_dune.ml
@@ -113,6 +113,8 @@ let () =
   let tests =
     [ "basic", enabled_if_main;
       "basic_u", enabled_if_main;
+      "ops_float32x4", enabled_if_main;
+      "ops_float32x4_u", enabled_if_main;
       "ops", enabled_if_main_amd64_not_macos;
       "ops_u", enabled_if_main_amd64_not_macos;
       "arrays", enabled_if_main;
