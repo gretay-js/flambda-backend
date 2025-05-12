e450949be859ab11d03c49cf2f824b5982ed6558
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-05-12 12:18:01 +0100

    fix

--- flambda-backend/tests/simd/amd64/builtins.ml
+++ flambda-backend/tests/simd/amd64/builtins.ml
@@ -7,6 +7,14 @@ module Float64 = struct
   external min : t -> t -> t = "" "caml_sse2_float64_min"
     [@@noalloc] [@@builtin] [@@unboxed]
 
+  external max_match_sse : t -> t -> t
+    = "caml_vec128_unreachable" "caml_simd_float64_max"
+    [@@noalloc] [@@builtin] [@@unboxed]
+
+  external min_match_sse : t -> t -> t
+    = "caml_vec128_unreachable" "caml_simd_float64_min"
+    [@@noalloc] [@@builtin] [@@unboxed]
+
   external sqrt : t -> t = "" "caml_sse2_float64_sqrt"
     [@@noalloc] [@@builtin] [@@unboxed]
 
