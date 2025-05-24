313119907db4b1082e7c321308b923c6e4ba6776
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-05-24 10:13:36 +0100

    Enable ops_float64x2_u tests

--- flambda-backend/tests/simd/gen/gen_dune.ml
+++ flambda-backend/tests/simd/gen/gen_dune.ml
@@ -116,7 +116,7 @@ let () =
       "ops_float32x4", enabled_if_main;
       "ops_float32x4_u", enabled_if_main;
       "ops_float64x2", enabled_if_main;
-      "ops_float64x2_u", enabled_if_main_amd64_not_macos;
+      "ops_float64x2_u", enabled_if_main;
       "ops", enabled_if_main_amd64_not_macos;
       "ops_u", enabled_if_main_amd64_not_macos;
       "arrays", enabled_if_main;
