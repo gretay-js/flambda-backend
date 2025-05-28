b9514cb5d0b45519be5fa0306513787a5a2bdb9c
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-05-28 15:38:11 +0200

    tmp

--- flambda-backend/tests/simd/gen/gen_dune.ml
+++ flambda-backend/tests/simd/gen/gen_dune.ml
@@ -117,8 +117,16 @@ let () =
       "ops_float32x4_u", enabled_if_main;
       "ops_float64x2", enabled_if_main;
       "ops_float64x2_u", enabled_if_main;
+      "ops_int64x2", enabled_if_main;
+      "ops_int64x2_u", enabled_if_main;
+      "ops_int32x4", enabled_if_main;
+      "ops_int32x4_u", enabled_if_main;
+      "ops_int16x8", enabled_if_main;
+      "ops_int16x8_u", enabled_if_main;
+      "ops_int8x16", enabled_if_main;
+      "ops_int8x16_u", enabled_if_main;
       "ops", enabled_if_main;
-      "ops_u", enabled_if_main_amd64_not_macos;
+      "ops_u", enabled_if_main;
       "arrays", enabled_if_main;
       "arrays_u", enabled_if_main;
       "scalar_ops", enabled_if_main;
